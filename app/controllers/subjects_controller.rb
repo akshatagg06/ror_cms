14f9ddcb1c354e4d749b2911bd3be3d1591ad884
akshatagg06 <akshat.agg1506@gmail.com>
2021-09-21 14:45:06 +0530

    Added controller filters: pages_controller --> find_subjects, set_page_count sections_controller --> find_pages, set_section_count subjects_controller --> set_subject_controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,7 +2,10 @@ class SubjectsController < ApplicationController
 
   layout 'admin'
 
+  before_action :set_subject_count, :only => [:new, :create, :edit, :update]
+
   def index
+    logger.debug("*** Testing the logger. ***")
     @subjects = Subject.sorted
   end
 
@@ -12,7 +15,6 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new({:name => 'Default'})
-    @subject_count = Subject.count + 1
   end
 
   def create
@@ -25,14 +27,12 @@ class SubjectsController < ApplicationController
       redirect_to(subjects_path)
     else
       # If save fails, redisplay the form so user can fix problems
-      @subject_count = Subject.count + 1
       render('new')
     end
   end
 
   def edit
     @subject = Subject.find(params[:id])
-    @subject_count = Subject.count
   end
 
   def update
@@ -45,7 +45,6 @@ class SubjectsController < ApplicationController
       redirect_to(subject_path(@subject))
     else
       # If save fails, redisplay the form so user can fix problems
-      @subject_count = Subject.count
       render('edit')
     end
   end
@@ -65,6 +64,13 @@ class SubjectsController < ApplicationController
 
   def subject_params
     params.require(:subject).permit(:name, :position, :visible, :created_at)
+  end
+
+  def set_subject_count
+    @subject_count = Subject.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @subject_count += 1
+    end
   end
 
 end
